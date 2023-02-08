614624442eb7bc1c2c4db9f11278324d0cd7a28b
Emanuele-Sgroi <emanuelesgroi.dev@gmail.com>
2023-02-08 20:57:48 +0000

    updated

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from the server, bot's response
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://codex-ai-bot.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
